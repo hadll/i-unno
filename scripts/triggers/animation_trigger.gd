e766f24208382b534547481419bd4ae24308ac37
Haddle <94085928+hadll@users.noreply.github.com>
2026-06-29 02:45:38 +1000

    sorta fix for spammed animation trigger

--- scripts/triggers/animation_trigger.gd
+++ scripts/triggers/animation_trigger.gd
@@ -10,6 +10,10 @@ class_name AnimationTrigger
 @export_category("Settings")
 @export var force_animation := false
 
+@export var limit_copies := true
+@export var start_limit := 1
+@export var end_limit := 2
+
 func _ready() -> void:
 	for i_trigger in triggers:
 		i_trigger.trigger_start.connect(on_trigger_start)
@@ -23,6 +27,12 @@ func on_trigger_end(trigger:Trigger):
 
 func play_animation(animation_name:String):
 	if not force_animation:
+		var copies = 0
+		for animation in animation_player.get_queue():
+			if animation == animation_name:
+				copies+=1
+		if (copies >= start_limit and animation_name == start_animation_name) or (copies >= end_limit and animation_name == end_animation_name):
+			return
 		animation_player.queue(animation_name)
 	else:
 		animation_player.stop()
