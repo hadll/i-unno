b4e94825b3e1a1865e07622de2408dc38135bc47
chloegregg <charliejamesgregg@gmail.com>
2026-07-01 18:49:54 +1000

    terminal arrow keys

--- scripts/terminal/terminal_display.gd
+++ scripts/terminal/terminal_display.gd
@@ -15,6 +15,9 @@ var typing_start_text_index := 0
 var typing_start_string_index := 0
 var allow_typing := true
 var printing := false
+var command_history: Array[String] = []
+var command_history_index := 0
+var cursor_inset_amount := 0
 
 func _ready() -> void:
 	if MultiplayerConnection.username:
@@ -42,24 +45,58 @@ func key_input(event: InputEventKey) -> void:
 	if OS.is_keycode_unicode(event.keycode):
 		if allow_typing:
 			var character := String.chr(event.unicode)
-			screen_text.add_text(character)
 			if character == "[":
-				screen_string += "[lb]"
+				screen_string = screen_string.substr(0, len(screen_string) - cursor_inset_amount) + "[lb]" + screen_string.substr(len(screen_string) - cursor_inset_amount)
 			else:
-				screen_string += character
+				screen_string = screen_string.substr(0, len(screen_string) - cursor_inset_amount) + character + screen_string.substr(len(screen_string) - cursor_inset_amount)
+			if cursor_inset_amount == 0:
+				screen_text.add_text(character)
+			else:
+				update_display_text()
 	elif event.keycode == Key.KEY_BACKSPACE:
-		if len(screen_string) > typing_start_string_index:
-			if screen_string.substr(len(screen_string) - 4) == "[lb]":
-				screen_string = screen_string.substr(0, len(screen_string) - 4)
+		if len(screen_string) - cursor_inset_amount > typing_start_string_index:
+			if screen_string.substr(len(screen_string) - cursor_inset_amount - 4, 4) == "[lb]":
+				screen_string = screen_string.substr(0, len(screen_string) - cursor_inset_amount - 4) + screen_string.substr(len(screen_string) - cursor_inset_amount)
 			else:
-				screen_string = screen_string.substr(0, len(screen_string) - 1)
+				screen_string = screen_string.substr(0, len(screen_string) - cursor_inset_amount - 1) + screen_string.substr(len(screen_string) - cursor_inset_amount)
 			screen_text.text = "clear" # force a regeneration
-			screen_text.text = screen_string
+			update_display_text()
 	elif event.keycode == Key.KEY_ENTER:
 		var command := screen_string.substr(typing_start_string_index).replace("[lb]", "[")
 		print_text("\n")
 		finish_printing()
 		Terminal.run(command)
+		command_history.append(command)
+		command_history_index = len(command_history)
+		cursor_inset_amount = 0
+	elif event.keycode == Key.KEY_UP:
+		command_history_index = maxi(0, command_history_index - 1)
+		screen_string = screen_string.substr(0, typing_start_string_index)
+		screen_string += command_history[command_history_index].replace("[", "[lb]")
+		cursor_inset_amount = 0
+		update_display_text()
+	elif event.keycode == Key.KEY_DOWN:
+		command_history_index = mini(len(command_history), command_history_index + 1)
+		screen_string = screen_string.substr(0, typing_start_string_index)
+		if command_history_index < len(command_history):
+			screen_string += command_history[command_history_index].replace("[", "[lb]")
+		cursor_inset_amount = 0
+		update_display_text()
+	elif event.keycode == Key.KEY_LEFT:
+		if screen_string.substr(len(screen_string) - cursor_inset_amount - 4, 4) == "[lb]":
+			cursor_inset_amount += 4
+		else:
+			cursor_inset_amount += 1
+		cursor_inset_amount = mini(len(screen_string) - typing_start_string_index, cursor_inset_amount)
+	elif event.keycode == Key.KEY_RIGHT:
+		if screen_string.substr(len(screen_string) - cursor_inset_amount, 4) == "[lb]":
+			cursor_inset_amount -= 4
+		else:
+			cursor_inset_amount -= 1
+		cursor_inset_amount = maxi(0, cursor_inset_amount)
+
+func update_display_text() -> void:
+	screen_text.text = screen_string
 
 func print_text(text: String, bbcode_start := "", bbcode_end := "") -> void:
 	if not printing:
