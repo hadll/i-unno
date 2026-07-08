f6606c5e959ecda8168eb383c1afb0c99020ba39
chloegregg <charliejamesgregg@gmail.com>
2026-07-09 01:31:57 +1000

    more objects spawners

--- scripts/levelgen/room.gd
+++ scripts/levelgen/room.gd
@@ -1,7 +1,6 @@
 class_name Room
 extends GenerationObject
 
-@export var objects: Array[GenerationObject]
 @export_group("Material Meshes", "meshes_")
 @export var meshes_wall: Array[MeshInstance3D]
 @export var meshes_floor: Array[MeshInstance3D]
@@ -11,5 +10,6 @@ func generate(section_def: SectionDef, rng: RandomNumberGenerator) -> void:
 		mesh.material_override = section_def.material_wall
 	for mesh in meshes_floor:
 		mesh.material_override = section_def.material_floor
-	for object in objects:
-		object.generate(section_def, rng)
+	for child in get_children():
+		if child is GenerationObject:
+			child.generate(section_def, rng)
