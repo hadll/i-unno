1971d16973891fb4b8cf4475fb54431d70d339e1
chloegregg <charliejamesgregg@gmail.com>
2026-07-08 22:17:28 +1000

    change generation object picker to dictionary

--- scripts/levelgen/generation_object_picker.gd
+++ scripts/levelgen/generation_object_picker.gd
@@ -1,22 +1,21 @@
 class_name GenerationObjectPicker
 extends GenerationObject
 
-@export var scenes: Array[PackedScene]
-@export var weights: Array[float]
+@export var scenes: Dictionary[PackedScene, float]
 
 var picked: GenerationObject
 
 func generate(section_def: SectionDef, rng: RandomNumberGenerator) -> void:
 	var total := 0.0
-	for weight in weights:
+	for weight in scenes.values():
 		total += weight
 	var roll := rng.randf() * total
-	for i in len(weights):
-		roll -= weights[i]
+	for scene in scenes:
+		roll -= scenes[scene]
 		if roll < 0.0:
-			if not scenes[i]:
+			if not scenes[scene]:
 				break
-			picked = scenes[i].instantiate()
+			picked = scene.instantiate()
 			add_child(picked)
 			picked.generate(section_def, rng)
 			break
