753d61d4a331fa4fb5453e98a8b54e849026a986
Haddle <94085928+hadll@users.noreply.github.com>
2026-07-08 10:15:50 +1000

    yeah brains not workin with this

--- scripts/item.gd
+++ scripts/item.gd
@@ -2,6 +2,8 @@
 class_name Item
 extends Node3D
 
+signal held_sprite_update
+
 @abstract
 func get_item_name() -> String
 
@@ -10,3 +12,9 @@ func get_item_id() -> StringName
 
 func item_input(_event: InputEvent) -> void:
 	pass
+
+@abstract
+func get_item_icon() -> Image
+
+@abstract
+func get_item_held_sprite() -> Image
