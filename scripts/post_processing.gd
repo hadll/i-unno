1f614540959cd712d0461f6b40fe493e3b3d65e1
chloegregg <charliejamesgregg@gmail.com>
2026-07-10 22:08:47 +1000

    fix ui input propagation

--- scripts/post_processing.gd
+++ scripts/post_processing.gd
@@ -16,6 +16,8 @@ func _ready() -> void:
 	RenderingServer.viewport_attach_camera(game_viewport.get_viewport_rid(), PlayerCamera.get_camera_rid())
 	get_window().size_changed.connect(update_size)
 	update_size()
+	
+	InputHandler.input.connect(game_viewport.push_input)
 
 func update_size() -> void:
 	game_viewport.size = get_window().size
