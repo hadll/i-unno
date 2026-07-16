cd1bff1a0adcef4b9efc6d42cbacd8c064938530
chloegregg <charliejamesgregg@gmail.com>
2026-07-17 00:39:56 +1000

    abyss gets larger when insane

--- scripts/enemies/enemy_abyss.gd
+++ scripts/enemies/enemy_abyss.gd
@@ -6,6 +6,7 @@ static var closest_dist: float
 
 @export var audio_player: AudioStreamPlayer3D
 @export var wait_timer: Timer
+@export var mesh: MeshInstance3D
 @export var min_wait_time: float
 @export var max_wait_time: float
 @export var speed: float
@@ -13,6 +14,7 @@ static var closest_dist: float
 @export var float_height: float
 @export var size: float
 @export var sanity_drain: float
+@export var size_gain: float
 @export var distortion_distance: float
 @export var distortion_falloff: float
 @export var distortion_strength: float
@@ -36,10 +38,13 @@ func _process(delta: float) -> void:
 	var distance := offset.length()
 	var distortion := maxf(0.0, 1.0 - pow(distance / distortion_distance, distortion_falloff))
 	var screen_pos := PlayerCamera.unproject_position(global_position)
-	var centre := (screen_pos / Vector2(get_window().size)).clampf(0.0, 1.0)
+	var centre := (screen_pos / Vector2(get_window().size))
+	var on_screen := centre.x >= 0.0 and centre.x <= 1.0 and centre.y >= 0.0 and centre.y <= 1.0
+	centre = centre.clampf(0.0, 1.0)
 	var camera_angle_factor := maxf(0.0, PlayerCamera.global_basis.z.angle_to(offset) / TAU * 4 - 1.0)
 	
-	Player.me.reduce_sanity(distortion * camera_angle_factor * sanity_drain * delta)
+	if on_screen:
+		Player.me.reduce_sanity(distortion * camera_angle_factor * sanity_drain * delta)
 	
 	if not closest or closest == self or distance < closest_dist:
 		closest = self
@@ -49,6 +54,8 @@ func _process(delta: float) -> void:
 		mat.set_shader_parameter(&"centre", centre)
 	
 	generate_audio(distortion)
+	
+	mesh.scale = Vector3.ONE * (size + size_gain * (1.0 - Player.me.calmness))
 
 func _physics_process(delta: float) -> void:
 	var offset := target_position - global_position
